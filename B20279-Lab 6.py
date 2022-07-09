7ad5f371d0db99b8919c4ddcb344f254dd9e1ea3
AryanA3735 <98833258+AryanA3735@users.noreply.github.com>
2022-07-09 12:52:20 +0530

    Update B20279-Lab 6.py

--- B20279-Lab 6.py
+++ B20279-Lab 6.py
@@ -1,163 +1,265 @@
-# Aryan Ali
-# B20279
-# 9027209190
+'''
+NAME - Aryan Ali
+BRANCH - DSE
+ROLL NO. - B20279
+MOBILE NO. - 9027209190
+'''
 
-from statsmodels.tsa.ar_model import AutoReg as AR
+#%% Importing libraries
 import math
-from statsmodels.graphics import tsaplots
-import statsmodels.api as sm
-import plotly.graph_objects as go
+from statsmodels.tsa.ar_model import AutoReg
+from sklearn.metrics import mean_squared_error
+from statsmodels.graphics.tsaplots import plot_acf
+from scipy.stats import pearsonr
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 
-# Q1(a)-----------------------------------------------------------------------------------------------------------------
-df = pd.read_csv('daily_covid_cases.csv')
-df['Date'] = pd.to_datetime(df['Date'], format='%d-%m-%Y')
-fig = go.Figure([go.Scatter(x=df['Date'], y=df['new_cases'])])
-fig.update_xaxes(title_text='Month-Year', dtick="M1", tickformat="%b<br>%Y")
-fig.update_yaxes(title_text='New confirmed cases')
-fig.show()
-# (b)--------------------------------------------------------------
-df_lag = df.drop([0], axis=0)
-df_org = df.drop([611], axis=0)
-a_corr = np.corrcoef(df_org['new_cases'], df_lag['new_cases'])
-print(a_corr)
-# (c)--------------------------------------------------------------
-plt.scatter(df_org['new_cases'], df_lag['new_cases'])
-plt.title('Scatter plot between original and lagged dataframe')
-plt.show()
-# (d)--------------------------------------------------------------
-lag = [1, 2, 3, 4, 5, 6]
-ac = sm.tsa.acf(df['new_cases'], nlags=6)
-ac = np.delete(ac, 0)
-print(ac)
-plt.plot(lag, ac, '-ro')
-plt.xlabel('Lag')
-plt.ylabel('Auto-correlation')
-plt.show()
-# (e)---------------------------------------------------------------
-fig5 = tsaplots.plot_acf(df['new_cases'], lags=6)
+#%% Question 1
+print(" ___________________________________Question 1_________________________________")
+df = pd.read_csv("daily_covid_cases.csv")
+df['Date'] = pd.to_datetime(df['Date'],infer_datetime_format=True)
+#%% Part A
+print('Part A')
+# Plotting the Data
+x = ['Feb-20','Apr-20','Jun-20','Aug-20','Oct-20','Dec-20','Feb-21','Apr-21','Jun-21','Aug-21','Oct-21']
+fig, ax = plt.subplots()
+ax.plot(df['new_cases'])
+ax.set_xticks(np.linspace(0,612,11))
+ax.set_xticklabels(x)
+ax.set_xlabel("Month-Year")
+ax.set_ylabel("New confirmed cases")
+#plt.savefig('Q1a.eps', format = 'eps')
 plt.show()
 
-# Q2(a)-----------------------------------------------------------------------------------------------------------------
-series = pd.read_csv('daily_covid_cases.csv', index_col=['Date'])
-test_size = 0.35  # 35% for testing
+#%% Part B
+print()
+print('Part B')
+# Shifting the data by 1 day
+# Computing the pearson correlation coefficient
+df1bi = df.iloc[1: , :]
+df1bii = df.set_index(["Date"]).shift(1)
+df1bii = df1bii.dropna()
+corr, _ = pearsonr(df1bi['new_cases'], df1bii['new_cases'])
+print('Pearson correlation (autocorrelation) coefficient: %.3f' % corr)
+
+#%% Part C
+print()
+print('Part C')
+# Plotting the lagged vs given sequence
+plt.scatter(df1bii['new_cases'],df1bi['new_cases'], color = 'b')
+plt.xlabel("Given time sequence")
+plt.ylabel("One day lag sequence")
+#plt.savefig('Q1c.eps', format = 'eps')
+plt.show()
+
+#%% Part D
+print()
+print('Part D')
+# Computing the correlation coefficient for lag 1-6
+# Storing it into a list
+# Ploting the correlation coefficient
+l = []
+def autocorr(n):
+    df1di = df.iloc[n: , :]
+    df1dii = df.set_index(["Date"]).shift(n)
+    df1dii = df1dii.dropna()
+    corr, _ = pearsonr(df1di['new_cases'], df1dii['new_cases'])
+    l.append(corr)
+    print('Pearson correlation (autocorrelation) coefficient (lag = ',n,'): ',round(corr,3))
+
+for i in range(1,7):
+    autocorr(i)
+plt.plot(np.linspace(1,6,6),l)
+plt.xlabel("Lag values")
+plt.ylabel("Correlation coefficient")
+#plt.savefig('Q1d.eps', format = 'eps')
+plt.show()
+
+#%% Part E
+# Plotting the correlation coefficient using plot.acf
+print()
+print('Part E')
+df1e = df.set_index(["Date"])
+plot_acf(df1e, lags=5)
+plt.xlabel("Lag values")
+plt.ylabel("Correlation coefficient")
+#plt.savefig('Q1e.eps', format = 'eps')
+plt.show()
+
+
+#%% Question 2
+print(" ___________________________________Question 2_________________________________")
+
+#%% Part A
+# Splitting our dataset into training and testing (75-35 split)
+print()
+print('Part A')
+series = pd.read_csv('daily_covid_cases.csv', parse_dates=['Date'], index_col=['Date'],sep=',')
+test_size = 0.35 # 35% for testing
 X = series.values
 tst_sz = math.ceil(len(X)*test_size)
 train, test = X[:len(X)-tst_sz], X[len(X)-tst_sz:]
 
-window = 5  # The lag=5
-model = AR(train, lags=window)
-model_fit = model.fit()  # fit/train the model
+# Plotting the testing and training data
+fig, ax = plt.subplots()
+ax.plot(train, label='Train Data')
+ax.plot(test, label='Test Data')
+ax.legend()
+plt.show()
+
+# Building AR model for lags = 5
+# Printing the obtained weight coefficients
+window = 5
+model = AutoReg(train, lags=5)
+model_fit = model.fit()
 coef = model_fit.params
-print(coef)
-# (b)-------------------------------------------------------------------------
+print("The coefficients obtained from the AR model are :",coef)
+
+#%% Part B
+print()
+print('Part B')
+# Taking 5 lag values (independent variable)
+# Multiplying with the weights and computing the dependent variable (t value)
+# walk forward over time steps in test
 history = train[len(train)-window:]
 history = [history[i] for i in range(len(history))]
-predictions = list()  # List to hold the predictions, 1 step at a time
+predictions = list()
+exp = list()
+for t in range(len(test)):
+	length = len(history)
+	lag = [history[i] for i in range(length-window,length)]
+	yhat = coef[0]
+	for d in range(window):
+		yhat += coef[d+1] * lag[window-d-1]
+	obs = test[t]
+	predictions.append(yhat)
+	exp.append(obs)
+	history.append(obs)
+
+#%% Part B (i)
+# Plotting the actual vs predicted series
+print('Part B (i)')
+plt.scatter(exp,predictions)
+plt.xlabel("Actual values")
+plt.ylabel("Predicted values")
+#plt.savefig('Q2bi.eps', format = 'eps')
+plt.show()
+
+#%% Part B (ii)
+print('Part B (ii)')
+# Plotting the actual vs predicted series on same plot
+fig, ax = plt.subplots()
+ax.plot(test, label="Actual data")
+ax.plot(predictions, label="Predicted data")
+ax.set_xticks(np.linspace(0,214,3))
+ax.set_xticklabels(x[8:])
+ax.set_xlabel("Month-Year")
+ax.set_ylabel("New confirmed cases")
+plt.legend()
+#plt.savefig('Q2bii.eps', format = 'eps')
+plt.show()
+
+
+#%% Part B (iii)
+print('Part B (ii)')
+# Computing RMSE(%) and MAPE value
+rmse = math.sqrt(mean_squared_error(exp, predictions))/(np.mean(exp))
+print("RMSE(%):", round(rmse*100,3))
+
+mape = np.mean(np.abs((np.array(exp) - np.array(predictions))/np.array(exp)))*100
+print("MAPE:", round(mape,3))
+
+#%% Question 3
+print()
+print(" ___________________________________Question 3_________________________________")
+# Similar to above question repeating it for lag 1, 5, 10, 15, 25.
+# Plotting bar graphs for for RMSE(%) and MAPE error
+l = [1, 5, 10, 15, 25]
+rm = []
+ma = []
+for window in l:
+	model = AutoReg(train, lags=window)
+	model_fit = model.fit()
+	coef = model_fit.params
+	history = train[len(train) - window:]
+	history = [history[i] for i in range(len(history))]
+	predictions = list()
+	exp = list()
+	for t in range(len(test)):
+		length = len(history)
+		lag = [history[i] for i in range(length - window, length)]
+		yhat = coef[0]
+		for d in range(window):
+			yhat += coef[d + 1] * lag[window - d - 1]
+		obs = test[t]
+		predictions.append(yhat)
+		exp.append(obs)
+		history.append(obs)
+	rmse = math.sqrt(mean_squared_error(exp, predictions)) / (np.mean(exp))
+	rm.append(round(rmse*100,3))
+	mape = np.mean(np.abs((np.array(exp) - np.array(predictions)) / np.array(exp))) * 100
+	ma.append(round(mape,3))
+for i in range(len(l)):
+	print("RMSE(%) for lag ",l[i],":",rm[i])
+print()
+for i in range(len(l)):
+	print("MAPE for lag ",l[i],":",ma[i])
+
+plt.bar(l,rm, width = 1.5)
+plt.xlabel("Lag values")
+plt.ylabel("RMSE(%)")
+#plt.savefig('Q3i.eps', format = 'eps')
+plt.show()
+plt.bar(l,ma, width = 1.5)
+plt.xlabel("Lag values")
+plt.ylabel("MAPE")
+#plt.savefig('Q3ii.eps', format = 'eps')
+plt.show()
+
+#%% Question 4
+# Computing the heuristic value
+# Running our AR model for heurisic value
+# Obtaining the RMSE(%) and MAPE error
+print()
+print(" ___________________________________Question 4_________________________________")
+m = 2/np.sqrt(len(train))
+i = 1
+
+def autocorr(n):
+    train_t0 = [item for elem in train[n:] for item in elem]
+    train_tn = [item for elem in train[:(-1*n)] for item in elem]
+    corr, _ = pearsonr(train_t0,train_tn)
+    return corr
+while True:
+	if autocorr(i) > m:
+		i+=1
+	else:
+		break
+i-=1
+
+print ('The heuristic value for the optimal number of lags is: ',i)
+window = i
+model = AutoReg(train, lags=window)
+model_fit = model.fit()
+coef = model_fit.params
+history = train[len(train) - window:]
+history = [history[i] for i in range(len(history))]
+predictions = list()
+exp = list()
 for t in range(len(test)):
     length = len(history)
-    lag = [history[i] for i in range(length-window, length)]
-    yhat = coef[0]  # Initialize to w0
+    lag = [history[i] for i in range(length - window, length)]
+    yhat = coef[0]
     for d in range(window):
-        yhat += coef[d+1] * lag[window-d-1]  # Add other values
+        yhat += coef[d + 1] * lag[window - d - 1]
     obs = test[t]
-    predictions.append(yhat)  # Append predictions to compute RMSE later
-    history.append(obs)  # Append actual test value to history, to be used in next step.
-
-plt.scatter(test, predictions)
-plt.title('Actual vs Predictions')
-plt.xlabel('Actual')
-plt.ylabel('Predictions')
-plt.show()
-
-dates = df['Date']
-dates = dates.iloc[397:612]
-
-plt.plot(dates, test)
-plt.plot(dates, predictions)
-plt.xlabel('Date')
-plt.ylabel('New confirmed cases')
-plt.legend(['Actual Data', 'predicted Data'])
-plt.show()
-
-rmspe = np.sqrt(np.mean(np.square(((test - predictions) / test))))*100
-print('RMSPE: ', str(rmspe))
-def mape(actual, predic):
-    actual, predic = np.array(actual), np.array(predic)
-    return np.mean(np.abs((actual - predic) / actual)) * 100
+    predictions.append(yhat)
+    exp.append(obs)
+    history.append(obs)
+rmse = math.sqrt(mean_squared_error(exp, predictions)) / (np.mean(exp))
+print("RMSE(%) :",round(rmse*100,3))
+mape = np.mean(np.abs((np.array(exp) - np.array(predictions)) / np.array(exp))) * 100
+print("MAPE :",round(mape,3))
 
 
-map_err = mape(test, predictions)
-print('MAPE: ', str(map_err))
-
-# Q3--------------------------------------------------------------------------------------------------------------------
-rmse_err = []
-map_error = []
-lags = [1, 5, 10, 15, 25]
-def autoreg(n):
-    model3 = AR(train, lags=n)
-    model3_fit = model3.fit()
-    coef3 = model3_fit.params
-    history3 = train[len(train) - n:]
-    history3 = [history3[i] for i in range(len(history3))]
-    preds = list()
-    for k in range(len(test)):
-        length3 = len(history3)
-        lag3 = [history3[i] for i in range(length3 - n, length3)]
-        yhat3 = coef3[0]
-        for l in range(n):
-            yhat3 += coef3[l + 1] * lag3[n - l - 1]
-        obs3 = test[k]
-        preds.append(yhat3)
-        history3.append(obs3)
-    rmspe3 = np.sqrt(np.mean(np.square(((test - preds) / test)))) * 100
-    rmse_err.append(rmspe3)
-    mape3 = mape(test, preds)
-    map_error.append(mape3)
-
-
-for i in lags:
-    autoreg(i)
-plt.plot(lags, rmse_err, '-ro')
-plt.title('RMSE vs time-lag')
-plt.xlabel('time-lag')
-plt.ylabel('RMSE(%)')
-plt.show()
-plt.plot(lags, map_error, '-ro')
-plt.title('MAPE vs time-lag')
-plt.xlabel('time-lag')
-plt.ylabel('MAPE')
-plt.show()
-# Q4--------------------------------------------------------------------------------------------------------------------
-ac4 = sm.tsa.acf(train, nlags=100)
-ac4 = np.delete(ac4, 0)
-for i in range(len(ac4)):
-    if ac4[i] < 0.100:
-        print(i)
-    else:
-        continue
-
-model4 = AR(train, lags=78)
-model4_fit = model4.fit()
-coef4 = model4_fit.params
-
-history4 = train[len(train)-78:]
-history4 = [history4[i] for i in range(len(history4))]
-pred = list()
-for m in range(len(test)):
-    length4 = len(history4)
-    lag4 = [history4[i] for i in range(length4-78, length4)]
-    yhat4 = coef4[0]
-    for p in range(78):
-        yhat4 += coef4[p+1] * lag4[78-p-1]
-    obs4 = test[m]
-    pred.append(yhat4)
-    history4.append(obs4)
-
-rmspe4 = np.sqrt(np.mean(np.square(((test - pred) / test)))) * 100
-mape4 = mape(test, pred)
-print(rmspe4)
-print(mape4)
-# ----------------------------------------------------------------------------------------------------------------------
